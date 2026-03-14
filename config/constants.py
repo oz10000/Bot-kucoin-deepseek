fd9249f513acfc0627255f27ca4f572f1cefde06
oz10000 <ozdacat@gmail.com>
2026-03-14 15:58:14 -0300

    Create constants.py

---
+++ config/constants.py
@@ -0,0 +1,16 @@
+# Mapeo de timeframes a intervalos en segundos (para WebSocket)
+TIMEFRAME_TO_SECONDS = {
+    '1min': 60,
+    '3min': 180,
+    '5min': 300,
+    '15min': 900,
+    '30min': 1800,
+    '1hour': 3600,
+    '2hour': 7200,
+    '4hour': 14400,
+    '6hour': 21600,
+    '8hour': 28800,
+    '12hour': 43200,
+    '1day': 86400,
+    '1week': 604800,
+}
